454216bbb523c0a699f9d1a700af1b445ebdf519
adrhh <adrianhoryn@gmail.com>
2017-10-25 13:13:51 +0200

    Update e05.cpp

--- Pimer Plus/ch5/e05/e05/e05.cpp
+++ Pimer Plus/ch5/e05/e05/e05.cpp
@@ -1,12 +1,12 @@
 /* Daphne invests $100 at 10% simple interest.That is, every year, the investment earns
 10% of the original investment, or $10 each and every year:
-interest = 0.10  × original balance
+interest = 0.10  Ã— original balance
 At the same time, Cleo invests $100 at 5% compound interest.That is, interest is 5%
 of the current balance, including previous additions of interest:
-interest = 0.05  × current balance
+interest = 0.05  Ã— current balance
 Cleo earns 5% of $100 the first year, giving her $105.The next year she earns 5% of
 $105, or $5.25, and so on.Write a program that finds how many years it takes for
-the value of Cleo’s investment to exceed the value of Daphne’s investment and then
+the value of Cleoâ€™s investment to exceed the value of Daphneâ€™s investment and then
 displays the value of both investments at that time.*/
 
 #include <iostream>
@@ -39,7 +39,7 @@ int main()
 		cleo_current_balance += cleo_interest;
 	}
 
-	cout << "Po " << year++ << " latach Cleo zysk przekorczyl zysk Daph i wynosi:"
+	cout << "Po " << year << " latach Cleo zysk przekorczyl zysk Daph i wynosi:"
 		<< endl << setw(10) << setprecision(4) << "zysk Cleo: " << cleo_interest
 		<< "\t zysk Daph: " << daph_interest
 		<< endl;
