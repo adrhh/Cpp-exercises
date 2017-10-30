03ee34b5116551beb3e78bc1682c9c0fdbcc8286
adrhh <adrianhoryn@gmail.com>
2017-10-30 10:46:52 +0100

    Update e05.cpp

--- Pimer Plus/ch6/e05/e05/e05.cpp
+++ Pimer Plus/ch6/e05/e05/e05.cpp
@@ -4,8 +4,8 @@ First 5,000 tvarps: 0% tax
 Next 10,000 tvarps: 10% tax
 Next 20,000 tvarps: 15% tax
 Tvarps after 35,000: 20% tax
-For example, someone earning 38,000 tvarps would owe 5,000  × 0.00 + 10,000  ×
-0.10 + 20,000  × 0.15 + 3,000  × 0.20, or 4,600 tvarps.Write a program that uses a
+For example, someone earning 38,000 tvarps would owe 5,000  Ã— 0.00 + 10,000  Ã—
+0.10 + 20,000  Ã— 0.15 + 3,000  Ã— 0.20, or 4,600 tvarps.Write a program that uses a
 loop to solicit incomes and to report tax owed.The loop should terminate when
 the user enters a negative number or non-numeric input.*/
 
@@ -38,18 +38,18 @@ int main()
 			
 			if (reaming < FIRST)
 			{
-				total_tax += (float)FIRST_T;
+				total_tax += (float) FIRST_T;
 				break;
 			}
 			else
 			{
-				total_tax += (float)FIRST_T;
+				total_tax += (float) FIRST_T;
 				reaming -= FIRST;
 			}
 
 			if (reaming < SECOND)
 			{
-				total_tax += (float)reaming * SECOND_T;
+				total_tax += (float) reaming * SECOND_T;
 				break;
 			}
 			else
@@ -71,7 +71,7 @@ int main()
 
 			if (reaming)
 			{
-				total_tax += (float)reaming * FOURTH_T;
+				total_tax += (float) reaming * FOURTH_T;
 				reaming -= reaming;
 			}
 		}
