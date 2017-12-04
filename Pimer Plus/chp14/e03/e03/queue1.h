9135ec6eb9ec358ffade9ea6aa1b902b18914c56
adrhh <adrianhoryn@gmail.com>
2017-12-04 11:24:26 +0100

    up

---
+++ Pimer Plus/chp14/e03/e03/queue1.h
@@ -0,0 +1,24 @@
+#ifndef QUEUE_H_
+#define QUEUE_H_
+
+template <typename T>
+class Queue
+{
+private:
+	enum { QSIZE = 10 };
+	struct Node{T item; Node * next;};
+	Node* front;
+	Node* rear;
+	int items;
+	const int qsize;
+public:
+	Queue(int n = QSIZE);
+	~Queue();
+	bool isempty() const { return items == 0; }
+	bool isfull() const { return items == qsize; }
+	int queuecount() const { return items; }
+	bool enqueue(const T & item);
+	bool dequeue(T & item);
+};
+
+#endif //!QUEUE_H_
