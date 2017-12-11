2354363786947ec5d0662bd0d227563d22124c42
adrhh <adrianhoryn@gmail.com>
2017-12-11 17:35:40 +0100

    e01

---
+++ exercises/sql exercices/e01/e01/e01.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include "sqlite3.h"
+
+using std::wcerr;
+using std::endl;
+using std::cout;
+
+/*
+typedef int (*sqlite3_callback)(
+void*,     Data provided in the 4th argument of sqlite3_exec() 
+int,	   The number of columns in row 
+char**,    An array of strings representing fields in the row
+char**     An array of strings representing column names 
+); */
+
+static int callback(void* not_used, int columns, char **col_val, char **col_name)
+{
+	for (int i = 0; i<columns; i++) 
+		cout << col_name[i] << '\t' << col_val[i] ? col_val[i] : "NULL";
+
+	cout << endl;
+	return 0;
+}
+
+int main()
+{
+	sqlite3  *db;
+	bool sqlb;
+	char *err_msg = "blad bazy danych";
+
+	sqlb = sqlite3_open("test.db", &db);
+	if (sqlb)
+		wcerr << "Blad otwarcia bazy danych" << endl;
+	else
+		cout << "Baza otwarta" << endl;
+
+	//creat table
+	char *sql;
+	sql =
+		"CREATE TABLE empls"\
+		"("\
+		"id INTEGER PRIMARY KEY,"
+		"name VARCHAR(20) NOT NULL,"
+		"sname VARCHAR(30) NOT NULL,"
+		"salary DECIMAL(7, 2) NOT NULL,"
+		"position VARCHAR(20),"
+		"pesel CHAR(6)"
+		");";
+
+	sqlb = sqlite3_exec(db, sql, callback, 0, &err_msg );
+
+	if (sqlb != SQLITE_OK)
+	{
+		wcerr << err_msg;
+		sqlite3_free(err_msg);
+	}
+	else
+		cout << "tabela utworzona" << endl;
+
+	sqlite3_close(db);
+
+	return 0;
+}
