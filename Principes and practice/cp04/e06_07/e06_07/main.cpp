1e3cf1df63e9e42f80b8c9b089d6cd7f45142426
adrhh <adrianhoryn@gmail.com>
2017-12-14 18:39:28 +0100

    up

--- Principes and practice/cp04/e06_07/e06_07/main.cpp
+++ Principes and practice/cp04/e06_07/e06_07/main.cpp
@@ -6,18 +6,62 @@ using namespace std;
 
 //max 0-9
 int get_str_int(const string str);
+const vector<string> Digs = { "zero", "jeden", "dwa", "trzy",
+"cztery", "piec", "szesc", "siedem",
+"osiem", "dziewiec" };
+
 
 int main()
 {
-	const vector<string> Digs = { "zero", "jeden", "dwa", "trzy",
-							"cztery", "piec", "szesc", "siedem",
-							"osiem", "dziewiec" };
 
 	int in1;
 	int in2;
-	string in;
+	string sin1;
+	string sin2;
+	string sin;
+	char operat;
 	
+	cout << "wpisz dwie cyfry (slownie lub liczbowo) i operator dzialnia" << endl;
+	cout << "k- byz zakonczyc" << endl;
+	while (cin >> sin1 >> sin2 >> sin)
+	{
+		if (sin1[0] == 'k')
+			break;
+		try
+		{
+			in1 = get_str_int(sin1);
+			in2 = get_str_int(sin2);
+		}
+		catch (const char* s)
+		{
+			cout << "Wpisz: " << endl;
+			for (int i = 0; i < Digs.size(); i++)
+				cout << Digs[i] << ' ';
+			cout << endl;
+			continue;
+		}
 
+		operat = sin[0];
+
+		switch (operat)
+		{
+		case '+':
+			cout << in1 << " + " << in2 << " = " << in1 + in2 << endl;
+			break;
+		case '-':
+			cout << in1 << " - " << in2 << " = " << in1 - in2 << endl;
+			break;
+		case '/':
+			cout << in1 << " / " << in2 << " = " << in1 / in2 << endl;
+			break;
+		case '*':
+			cout << in1 << " * " << in2 << " = " << in1 * in2 << endl;
+			break;
+		default:
+			cout << "nie porawny operator " << endl;
+		}
+		cout << "wpisz dwie cyfry (slownie lub liczbowo) i operator dzialnia" << endl;
+	}
 
 
 	return 0;
@@ -25,14 +69,17 @@ int main()
 
 int get_str_int(const string str)
 {
-	int answ;
-	char temp= str[0];
-	if (isdigit(temp))
-		answ = atoi(temp);
+	int answ = -1;
+	if (isdigit(str[0]))
+		answ = stoi(str);
 	else
 	{
-
+		for (int i = 0; i < Digs.size(); i++)
+			if (str == Digs[i])
+				answ = i;
 	}
+	if (answ == -1)
+		throw "Niepoprawna cyfra";
 
-
+	return answ;
 }
