35cea4ad503f22da75190148dffb58db2f8128e6
adrhh <adrianhoryn@gmail.com>
2017-12-22 09:26:47 +0100

    e11 e12

--- Principes and practice/cp05/e11_e12/e11_e12/main.cpp
+++ Principes and practice/cp05/e11_e12/e11_e12/main.cpp
@@ -15,28 +15,42 @@ int bull_count(vector<int> f, vector<int> a);
 
 int main()
 {
-	srand(time(0));
+	srand((unsigned int)time(0));
 	int cchoice;
 	int hchoice=0;
 	vector<int> f;
 	vector<int> a;
 	int b, c;
 
-	int x = 100;
-	while (x--)
+	while (true)
 	{
 		cchoice = rand_i(4);
+		f = i_tov(cchoice);
 		cout << cchoice << endl;
+
 		while(hchoice != cchoice)
 		{ 
 		cin >> hchoice;
-		f = i_tov(cchoice);
 		a = i_tov(hchoice);
 		c = cow_count(f, a);
 		b = bull_count(f, a);
 		cout << cchoice << endl;
-		cout << c << " " << b << endl;
+		cout << "krowy: " << c << " byki: " <<  b << endl;
 		}
+
+		cout << "k-kontynnuj, z-zakoncz" << endl;
+		
+		char choice;
+		while (cin >> choice)
+		{
+			if (choice == 'k' || choice == 'z')
+				break;
+			cout << "wybierz: k - kontynuuj, z- zakoncz" << endl;
+			while (cin.get() != '\n')
+				continue;
+		}
+		if (choice == 'z')
+			break;
 
 	}
 	return 0;
@@ -61,11 +75,18 @@ int rand_i(int n)
 int cow_count(vector<int> f, vector<int> a)
 {
 	int cows = 0;
+	if (f.size() != a.size())
+		return -1;
 	for (int i = 0; i < a.size(); i++)
 	{
 		for (int j = 0; j < f.size(); j++)
 			if (a[i] == f[j] && i != j)
+			{
 				cows++;
+				f.erase(f.begin() + j);
+				f.insert(f.begin() + j, -1);
+				break;
+			}
 	}
 	return cows;
 }
@@ -73,6 +94,8 @@ int cow_count(vector<int> f, vector<int> a)
 int bull_count(vector<int> f, vector<int> a)
 {
 	int bulls = 0;
+	if (f.size() != a.size())
+		return -1;
 	for (int i = 0; i < a.size(); i++)
 		if (a[i] == f[i])
 				bulls++;
