aa650f7958eac8682bbbdf66e9a579a1c10c0100
adrhh <adrianhoryn@gmail.com>
2018-02-05 17:51:38 +0100

    e09

---
+++ Principes and practice/cp08/e09/e09/e09.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <ctime>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+long long seed;
+
+int LCG()
+{
+	const long long a = 48271;
+	const long long b = 1;
+	const long long m = (2 << 30) - 1;
+	seed = (a*seed + b) % m;
+	return (int)seed;
+}
+
+int randint(int min, int max)
+{
+	return min + LCG() %  (max - min);
+}
+
+int main()
+{
+
+	seed = time(0);
+
+	for (int i = 0; i < 100; i++)
+		cout << randint(5,10) << endl;
+
+	return 0;
+}
