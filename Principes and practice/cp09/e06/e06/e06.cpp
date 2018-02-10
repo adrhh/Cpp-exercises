8b026e392501df7f18e000a7866e034f3ab927bc
adrhh <adrianhoryn@gmail.com>
2018-02-10 19:35:33 +0100

    up

--- Principes and practice/cp09/e06/e06/e06.cpp
+++ Principes and practice/cp09/e06/e06/e06.cpp
@@ -34,12 +34,16 @@ public:
 	void change_brrd() { is_brred = !is_brred; }
 	class Invalid
 	{};
+	friend bool operator==(const Book& b1, const Book& b2);
+	friend bool operator!=(const Book& b1, const Book& b2);
+	friend ostream& operator<<(ostream& os, const Book& b);
 };
 
 
 int main()
 {
 	Book test(2034, 11451, 30324, 'z', "Pole", "Mikolaj");
+	cout << test;
 
 	return 0;
 }
@@ -72,3 +76,23 @@ bool operator==(const ISBN& i1, const ISBN& i2)
 	return answ;
 }
 
+
+bool operator==(const Book& b1, const Book& b2)
+{
+	return b1.isbn == b2.isbn;
+}
+
+bool operator!=(const Book& b1, const Book& b2)
+{
+	return !(b1.isbn == b2.isbn);
+}
+
+ostream& operator<<(ostream& os, const Book& b)
+{
+	os << b.author << endl;
+	os << b.title << endl;
+	os << b.isbn.ia << '-' << b.isbn.ib << '-'
+		<< b.isbn.ic << '-' << b.isbn.id;
+
+	return os;
+}
