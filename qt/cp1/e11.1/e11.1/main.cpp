5b596a89289458305baaac13f181745302797bbf
adrhh <adrianhoryn@gmail.com>
2018-02-25 13:31:14 +0100

    up

--- qt/cp1/e11.1/e11.1/main.cpp
+++ qt/cp1/e11.1/e11.1/main.cpp
@@ -15,18 +15,34 @@ int main(int argc, char *argv[])
 		factArg = QInputDialog::getInt(
 			0, "Kalkulator silni", "Silnia: ", 1
 		);
+
 		cout << "Uzytwkonik wprowadzil: " << factArg << endl;
+		if (factArg < 0)
+		{
+			QString response = QString("Wprowadzono liczbe ujemna.\n%1")
+				.arg("Kolejne obliczenie?");
+			answer = QMessageBox::question(0, "Jeszcze raz?", response,
+				QMessageBox::Yes | QMessageBox::No);
+			if (answer == QMessageBox::Yes)
+				continue;
+			else
+				break;
+		}
+
 		int i = 2;
 		while (i <= factArg)
 		{
 			fact *= i;
 			i++;
 		}
+		
 		QString response = QString("%1 silnia to %2.\n%3")
 			.arg(factArg).arg(fact)
 			.arg("Kolejne obliczenie?");
 		answer = QMessageBox::question(0, "Jeszcze raz?", response,
 			QMessageBox::Yes | QMessageBox::No);
+		if (answer == QMessageBox::No)
+			break;
 	} while (answer == QMessageBox::Yes);
 
 	return EXIT_SUCCESS;
