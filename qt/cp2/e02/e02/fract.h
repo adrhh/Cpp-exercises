783529e78b2b2cb0e284f02e570a6cb713256311
adrhh <adrianhoryn@gmail.com>
2018-03-06 19:42:52 +0100

    e02 undone

---
+++ qt/cp2/e02/e02/fract.h
@@ -0,0 +1,21 @@
+#ifndef FRACT_H_
+#define FRACT_H_
+
+#include <QString>
+
+class Fraction
+{
+private:
+	int numer;
+	int deno;
+public:
+	void set(int n, int m);
+	QString toString() const;
+	double toDouble() const;
+	Fraction add(const Fraction& fr);
+	Fraction sub(const Fraction& fr);
+	Fraction mult(const Fraction& fr);
+	Fraction div(const Fraction& fr);
+}
+
+#endif // !FRACT_H_
