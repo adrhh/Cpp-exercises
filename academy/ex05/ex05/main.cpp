65e35064d23c405b41681d78bfa9bdaa42eb4db6
adrhh <adrianhoryn@gmail.com>
2018-04-25 10:20:48 +0200

    test

--- academy/ex05/ex05/main.cpp
+++ academy/ex05/ex05/main.cpp
@@ -7,10 +7,12 @@ int main()
 {
 	BigInt t1 = BigInt("123");
 	BigInt t2 = BigInt("399");
+	BigInt t4 = BigInt("9439074309749307904370943790437904379047309743097403772871258612862178783687368736287682368739678263786328768723678362");
 	BigInt t3 = t1 + t2;
 	t1.showBigInt();
 	cout << endl;
 	t3.showBigInt();
+	t4.showBigInt();
 
 	return 0;
 }
