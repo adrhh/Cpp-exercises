c573357cfc6768ccdf77b4e44c89188d5e97ae22
adrhh <adrianhoryn@gmail.com>
2018-08-08 17:31:22 +0200

    Update main.cpp

--- exercises/graph/main.cpp
+++ exercises/graph/main.cpp
@@ -1,26 +1,36 @@
 #include "graf.h"
-;
 	
 int main()
 {
-	Graph test(5);
-	test.addEdge(0, 1);
-	test.addEdge(0, 2);
-	test.addEdge(0, 3);
-	test.addEdge(0, 4);
-	test.addEdge(1, 2);
-	test.addEdge(1, 4);
-	test.addEdge(3, 4);
-	test.printGraph();
+	//gereuj graf randGraphGerator(i, j)
+	//i - liczba wierzcholkow
+	//j - liczba krawedzi
+	Graph* test = randGraphGerator(17, 24);
+	test->printGraph();
 
-	test.greedyColoring();
-	test.printGraphColors();
-	test.resetColors();
-	test.printGraphColors();
-	test.exactColoring();
-	test.printGraphColors();
+	clock_t start, end;
 
-	getchar();
+	start = clock();
+	test->greedyColoring();
+	end = clock();
 
+	test->printGraphColors();
+
+	double greedyCloroingTime = (static_cast<double> (end - start)) / CLOCKS_PER_SEC;
+
+
+	test->resetColors();
+	start = clock();
+	test->exactColoring();
+	end = clock();
+
+	test->printGraphColors();
+
+	double exactCloroingTime = (static_cast<double> (end - start)) / CLOCKS_PER_SEC;
+
+	std::cout << "Czas dzialania algorytmu zachlannego: " << greedyCloroingTime << std::endl
+		      << "Czas dzialania algorytmu dokladnego: " << exactCloroingTime << std::endl;
+
+	delete test;
 	return 0;
 }
