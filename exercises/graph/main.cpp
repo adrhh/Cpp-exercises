353fcd48f7855276d4ade4889e90735a225dde37
adrhh <adrianhoryn@gmail.com>
2018-08-09 17:19:54 +0200

    Update main.cpp

--- exercises/graph/main.cpp
+++ exercises/graph/main.cpp
@@ -5,7 +5,7 @@ int main()
 	//gereuj graf randGraphGerator(i, j)
 	//i - liczba wierzcholkow
 	//j - liczba krawedzi
-	Graph* test = randGraphGerator(17, 24);
+	Graph* test = randGraphGerator(10, 20);
 	test->printGraph();
 
 	clock_t start, end;
@@ -14,6 +14,7 @@ int main()
 	test->greedyColoring();
 	end = clock();
 
+	std::cout << "Kolorowanie:" << std::endl;
 	test->printGraphColors();
 
 	double greedyCloroingTime = (static_cast<double> (end - start)) / CLOCKS_PER_SEC;
@@ -24,12 +25,23 @@ int main()
 	test->exactColoring();
 	end = clock();
 
+	std::cout << "Kolorowanie:" << std::endl;
 	test->printGraphColors();
 
 	double exactCloroingTime = (static_cast<double> (end - start)) / CLOCKS_PER_SEC;
 
+	test->resetColors();
+	start = clock();
+	test->heurColoring();
+	end = clock();
+	double heurCloroingTime = (static_cast<double> (end - start)) / CLOCKS_PER_SEC;
+
+	std::cout << "Kolorowanie:" << std::endl;
+	test->printGraphColors();
+
 	std::cout << "Czas dzialania algorytmu zachlannego: " << greedyCloroingTime << std::endl
-		      << "Czas dzialania algorytmu dokladnego: " << exactCloroingTime << std::endl;
+		<< "Czas dzialania algorytmu dokladnego: " << exactCloroingTime << std::endl
+		<< "Czas dzialania algorytmu hwurystycznegi: " << heurCloroingTime << std::endl;
 
 	delete test;
 	return 0;
