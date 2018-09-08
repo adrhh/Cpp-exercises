e7ec1943279f8d04586ccb878cea7a31d2d41685
adrhh <adrianhoryn@gmail.com>
2018-09-08 13:54:52 +0200

    ch4 gwozdzie done

--- basic algorithms exercises/ch4/gwozdzie/gw.cpp
+++ basic algorithms exercises/ch4/gwozdzie/gw.cpp
@@ -34,20 +34,30 @@ void printVec(vector<int>& v)
     std::for_each(v.begin(), v.end(), [](int i){cout << i << ' ';});
 }
 
+int findLeader(vector<int>& v)
+{
+	vector<int> arrOfIdnex(v.size(), 0);
+	for(auto it = v.begin(); it != v.end(); ++it)
+		arrOfIdnex[*it]++;
+	return *max_element(arrOfIdnex.begin(), arrOfIdnex.end());
+}
+
+int valueCount(vector<int>& v, int val)
+{
+	int counter = 0;
+	for_each(v.begin(), v.end(), [&](int i){if(val == i)counter++;});
+	return counter;
+}
+
 int zadanie(vector<int>& v, int k)
 {
     selectionSort(v);
-    int maksymalnePowtorzenie = INT_MIN;
-    int ilePowtorzen = 0;
-    for(unsigned int i = 0; i < v.size() - k - 1; ++i)
-    {
-        if(v[i] == v[i+1])
-            ilePowtorzen++;
-        else
-            ilePowtorzen = 0;
-        maksymalnePowtorzenie = maksymalnePowtorzenie > ilePowtorzen ? maksymalnePowtorzenie : ilePowtorzen;
-    }
-    return v.size() < maksymalnePowtorzenie ? v.size() : maksymalnePowtorzenie;
+	int leader = findLeader(v);
+	int leaderCount = valueCount(v, leader);
+	int reaming = find(v.begin(), v.end(), leader) - v.begin();
+	int addToReturn = reaming < k ? reaming : k;
+
+	return leaderCount + addToReturn;
 }
 
 int main()
@@ -55,7 +65,10 @@ int main()
     vector<int> test {3, 3, 2, 3, 4, 5};
     int uderzenia = 2;
 
-    cout << zadanie(test, uderzenia) << endl;
+    //cout << findLeader(test) << endl;
+	//cout << valueCount(test, findLeader(test)) << endl;
+	
+	cout << zadanie(test, uderzenia) << endl;
 
     return 0;
 }
